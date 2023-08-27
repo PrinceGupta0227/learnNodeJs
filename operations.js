ea86df8501c943af40f78806a2150aa7de478cfa
Prince Gupta <Princeg0227@gmail.com>
2023-08-27 16:00:27 +0530

    first commit

---
+++ operations.js
@@ -0,0 +1,11 @@
+const operations = require('./index');
+
+console.log(operations.add(3, 4));
+
+console.log(operations.multiply(3,4));
+
+let a = 20;
+let b = 30;
+let c = 21;
+
+console.warn(a+b+c);
